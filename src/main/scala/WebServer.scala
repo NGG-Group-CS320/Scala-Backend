ddb7966ba944a68cf99983c47000b4048161e499
Aaron Weiss <aaronweiss74@gmail.com>
2016-12-20 13:04:29 -0500

    Completed line request and implemented request caching.

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -30,14 +30,30 @@ object WebServer extends App {
       }
     }
 
-  def wheelRequest(ids: Set[Int]): String = {
+  val wheelRequestCache: collection.mutable.Map[Set[Int], String] = collection.mutable.Map()
+  def wheelRequest(ids: Set[Int]): String = wheelRequestCache.get(ids) match {
+    case Some(res) => res
+    case None => {
+      wheelRequestCache += (ids -> wheelRequestInternal(ids))
+      wheelRequestCache(ids)
+    }
+  }
+
+  def wheelRequestInternal(ids: Set[Int]): String = {
     val subQuery = s"""SELECT MAX("from") FROM ${Database.config.scoresTable.get} WHERE systemid = ?"""
     val prepared = Database.prepare(s"""SELECT * FROM ${Database.config.scoresTable.get} WHERE systemid = ? AND "from" = ($subQuery)""")
 
-    def node(name: String, children: String): String = s"""{
-      "name": "$name",
-      "children": [$children]
-    }"""
+    def node(name: String, children: String, color: Option[String] = None): String = color match {
+      case Some(color) => s"""{
+        "name": "$name",
+        "color": "$color",
+        "children": [$children]
+      }"""
+      case None => s"""{
+        "name": "$name",
+        "children": [$children]
+      }"""
+    }
 
     val entries = ids.flatMap { id =>
       prepared.setInt(1, id)
@@ -51,10 +67,10 @@ object WebServer extends App {
     val quarterThree = entries.filter(entry => entry.healthScore >= 400 && entry.healthScore < 600)
     val quarterFour = entries.filter(entry => entry.healthScore >= 600 && entry.healthScore <= 800)
 
-    val nodeOne = node("0-200", quarterOne.map(_.toJSON).mkString(",\n"))
-    val nodeTwo = node("200-400", quarterTwo.map(_.toJSON).mkString(",\n"))
-    val nodeThree = node("400-600", quarterThree.map(_.toJSON).mkString(",\n"))
-    val nodeFour = node("600-800", quarterFour.map(_.toJSON).mkString(",\n"))
+    val nodeOne = node("0-200", quarterOne.map(_.toJSON).mkString(",\n"), Some("#ac2517"))
+    val nodeTwo = node("200-400", quarterTwo.map(_.toJSON).mkString(",\n"), Some("#b04c00"))
+    val nodeThree = node("400-600", quarterThree.map(_.toJSON).mkString(",\n"), Some("#da9a00"))
+    val nodeFour = node("600-800", quarterFour.map(_.toJSON).mkString(",\n"), Some("#19b75c"))
 
     val halfOne = node("0-400", Seq(nodeOne, nodeTwo).mkString(",\n"))
     val halfTwo = node("400-800", Seq(nodeThree, nodeFour).mkString(",\n"))
@@ -62,7 +78,16 @@ object WebServer extends App {
     s"[$halfOne, $halfTwo]"
   }
 
-  def lineRequest(ids: Set[Int]): String = {
+  val lineRequestCache: collection.mutable.Map[Set[Int], String] = collection.mutable.Map()
+  def lineRequest(ids: Set[Int]): String = lineRequestCache.get(ids) match {
+    case Some(res) => res
+    case None => {
+      lineRequestCache += (ids -> lineRequestInternal(ids))
+      lineRequestCache(ids)
+    }
+  }
+
+  def lineRequestInternal(ids: Set[Int]): String = {
     val prepared = Database.prepare(s"SELECT * FROM ${Database.config.scoresTable.get} WHERE systemid = ?")
 
     val systems = ids.map { id =>
@@ -82,7 +107,7 @@ object WebServer extends App {
 
     def node(pair: (Int, Set[ScoresEntry])): String = pair match {
       case (id, entries) => {
-        val data = entries.map(_.toJSONPoint).mkString(",\n")
+        val data = entries.toSeq.sortBy(_.toUnix).map(_.toJSONPoint).mkString(",\n")
         s"""{
           "id": "$id",
           "name": "System $id",
@@ -91,7 +116,8 @@ object WebServer extends App {
       }
     }
 
-    ???
+    val result = systems.map(node).mkString(",\n")
+    s"[$result]"
   }
 
   val bindingFuture = Http().bindAndHandle(route, "localhost", 8081)
